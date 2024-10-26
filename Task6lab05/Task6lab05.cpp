6dce75df050bed26e1447c86944b45185945a867
admin <admin@DESKTOP-8VQH0LB>
2024-10-26 01:01:35 -0700

    Add project files.

---
+++ Task6lab05/Task6lab05.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a;
+	cout << "ENTER THE INTGER :\n";
+	cin >> a;
+	for (int i = 1; i <= a; i++)
+	{
+		cout << i * i * i << endl;;
+	}
+
+	return 0;
+}
